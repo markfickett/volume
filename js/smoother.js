ead508fdfa887b8d38e3da7cc68a8d031e9e79f4
Mark Fickett <mark.fickett@gmail.com>
2012-10-03 20:36:34 -0400

    Adjust smoothing to stay in correct range.

--- js/smoother.js
+++ js/smoother.js
@@ -18,21 +18,25 @@ for (var i = 0; i < smoother.NUM_VALUES; i++) {
 smoother.currentIndex = 0;
 
 /**
- * Add a value to the smoothing window and get the new smoothed value.
+ * Adds a value to the smoothing window and gets the new weighted average.
  */
 smoother.updateAndGet = function(value) {
 	smoother.currentIndex = (smoother.currentIndex + 1) %
 			smoother.NUM_VALUES;
 	smoother.values[smoother.currentIndex] = value;
 
+	// Since this uses a cosine falloff, and the integral of cosine from
+	// 0 to PI/2 is 1.0, we can multiply each value's contribution by
+	// our dt so that the overall weight sums to 1. Thus our smoothed value
+	// remains in the correct range.
 	var t = 0;
 	var sum = 0;
 	for (var i = smoother.currentIndex; i >= 0; i--, t += smoother.DT) {
-		sum += Math.cos(t) * smoother.values[i];
+		sum += Math.cos(t) * smoother.values[i] * smoother.DT;
 	}
 	for (var i = smoother.NUM_VALUES - 1; i > smoother.currentIndex;
 			i--, t += smoother.DT) {
-		sum += Math.cos(t) * smoother.values[i];
+		sum += Math.cos(t) * smoother.values[i] * smoother.DT;
 	}
 	return sum;
 };
